cf728f377c9747e4ab230e6344219c9b46193b3c
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-22 20:01:32 -0300

    feat: add index

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+for (let i = 0; i < 5; i += 1) {
+  player1.levelUp();
+}
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+class Battles extends Battle {}
+
+function runBattles(battles: Battles[]): void {
+  battles.forEach((battle) => battle.fight());
+}
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
