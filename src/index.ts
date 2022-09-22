950bd34e6cd4807b68d193a17271197185ecb806
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-22 20:32:01 -0300

    fix: github evaluator

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import Character from './Character';
 import Dragon from './Dragon';
 import Monster from './Monster';
 
-// fix git evaluator
-
 const player1 = new Character('player1');
 const player2 = new Character('player2');
 const player3 = new Character('player3');
