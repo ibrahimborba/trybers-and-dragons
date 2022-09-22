476c6d52ac6f803ead4918a80c9a31011c9fd8c7
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-22 15:54:21 -0300

    feat: add Mage class

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static countInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Mage.countInstances += 1; 
+  }
+
+  get energyType(): EnergyType { return this._energyType; }
+
+  public static createdArchetypeInstances(): number {
+    return Mage.countInstances; 
+  }
+}
--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,5 @@
 import Archetype from './Archetype';
+import Mage from './Mage';
 
+export { Mage };
 export default Archetype;
